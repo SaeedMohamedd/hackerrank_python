dcbd0b1ed0479db11c3dc7ac75674fe5faee46af
SaeedMohamedd <saed01151599985@gmail.com>
2022-08-03 09:46:02 +0200

    without text wrap

--- textwrap.py
+++ textwrap.py
@@ -9,6 +9,7 @@ def wrap(string, max_width):
 
 
 
+#return "/n".join([string[i:i+max_width]for i in range(0,len(string),max_width)])
 #___________________mysolution______________________________________
 
 def wrap(string, max_width):
